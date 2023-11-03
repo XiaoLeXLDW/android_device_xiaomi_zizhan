5051ba90e6018f07edfbd4b3e25178671c3b244f
BuildTools <unconfigured@null.spigotmc.org>
2023-11-03 16:29:28 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -17,16 +17,17 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.0-impl.recovery \
+    android.hardware.boot@1.0-service \
 
 PRODUCT_PACKAGES += \
     bootctrl.taro
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.mSM8475 \
-    libgptutils \
-    libz \
-    libcutils
+# PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+#    bootctrl.mSM8475 \
+#    libgptutils \
+#    libz \
+#    libcutils
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
